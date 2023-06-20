398ef80eef079b8fc9fea647d41f56e0446ecdc1
nationalAcadem <alokc8502@gmail.com>
2023-06-20 18:06:47 +0530

    handle edit is changed

--- components/Students/studentTable.js
+++ components/Students/studentTable.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import { FiTrash2 } from 'react-icons/fi';
 import { FiEdit2 } from 'react-icons/fi';
@@ -80,67 +80,75 @@ const StudentTable = ({ reload }) => {
               </td>
 
               <td className='border border-black font-semibold px-2 py-2'>
-                {canChange?(<input
+                {canChange ? (<input
                   type="text"
                   value={item.name}
                   onChange={(e) => handleTableCellEdit(item_index, 'name', e.target.value)}
-                  className="border border-black px-2 py-2 w-full"
-                />):(item.name)}
-                
+                  className="border border-black px-1 py-1"
+                />) : (item.name)}
+
               </td>
               <td className='border border-black font-semibold px-2 py-2'>
-                {canChange?(<input
-                  type="text"
-                  value={item.standard}
-                  onChange={(e) => handleTableCellEdit(item_index, 'standard', e.target.value)}
-                  className="border border-black px-2 py-2 w-full"
-                />):(item.standard)}
+              {canChange ? (
+                  <select
+                    value={item.standard}
+                    onChange={(e) => handleTableCellEdit(item_index, 'standard', e.target.value)}
+                    className="border border-black px-1 py-1"
+                  >
+                    <option value="I" className='text-black'>XI</option>
+                    <option value="XII" className="text-black">XII</option>
+                  </select>
+                ) : (item.standard)}
               </td>
               <td className='border border-black font-semibold px-2 py-2'>
-                {canChange?(<input
-                  type="text"
-                  value={item.batch}
-                  onChange={(e) => handleTableCellEdit(item_index, 'batch', e.target.value)}
-                  className="border border-black px-2 py-2 w-full"
-                />):(item.batch)}
+                {canChange ? (
+                  <select
+                    value={item.batch}
+                    onChange={(e) => handleTableCellEdit(item_index, 'batch', e.target.value)}
+                    className="border border-black px-1 py-1"
+                  >
+                    <option value="Batch I" className='text-black'>Batch I</option>
+                    <option value="Batch II" className="text-black">Batch II</option>
+                  </select>
+                ) : (item.batch)}
               </td>
               <td className='border border-black font-semibold px-2 py-2'>
-                {canChange?(<input
+                {canChange ? (<input
                   type="text"
                   value={item.mobileNumber}
                   onChange={(e) => handleTableCellEdit(item_index, 'mobileNumber', e.target.value)}
-                  className="border border-black px-2 py-2 w-full"
-                />):(item.mobileNumber)}
+                  className="border border-black px-1 py-1"
+                />) : (item.mobileNumber)}
               </td>
               <td className='border border-black font-semibold px-2 py-2'>
-                {canChange?(<input
+                {canChange ? (<input
                   type="text"
                   value={item.address}
                   onChange={(e) => handleTableCellEdit(item_index, 'address', e.target.value)}
-                  className="border border-black px-2 py-2 w-full"
-                />):(item.address)}
+                  className="border border-black px-1 py-1"
+                />) : (item.address)}
               </td>
               <td className='border border-black font-semibold px-2 py-2'>
-                {canChange?(<input
+                {canChange ? (<input
                   type="text"
                   value={item.fees}
                   onChange={(e) => handleTableCellEdit(item_index, 'fees', e.target.value)}
-                  className="border border-black px-2 py-2 w-full"
-                />):item.fees}
+                  className="border border-black px-1 py-1"
+                />) : item.fees}
               </td>
               <td className="border border-black px-2">
                 <div className='flex space-x-1'>
-                <button 
-                  className="flex items-center justify-center rounded-md bg-green-500 hover:bg-blue-600 text-white py-1 px-2 font-semibold"
-                  onClick={() => setCanChange(true)}
-                >
-                  <FiEdit2 className="h-4 w-4" />
-                </button>
-                <button 
-                className="flex items-center justify-center rounded-md bg-red-500 hover:bg-blue-600 text-white py-1 px-2 font-semibold"
-                onClick={() => handleDeleteRow(item._id)}>
-                  <FiTrash2 className= "font-bold" />
-                </button>
+                  <button
+                    className="flex items-center justify-center rounded-md bg-green-500 hover:bg-blue-600 text-white py-1 px-2 font-semibold"
+                    onClick={() => setCanChange(true)}
+                  >
+                    <FiEdit2 className="h-4 w-4" />
+                  </button>
+                  <button
+                    className="flex items-center justify-center rounded-md bg-red-500 hover:bg-blue-600 text-white py-1 px-2 font-semibold"
+                    onClick={() => handleDeleteRow(item._id)}>
+                    <FiTrash2 className="font-bold" />
+                  </button>
                 </div>
               </td>
             </tr>
